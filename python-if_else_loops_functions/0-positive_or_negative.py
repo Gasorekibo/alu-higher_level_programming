c9e8fc9c7132a12c118bdb7bb73cc82d479eb229
Gasorekibo <m.gasore@alustudent.com>
2022-10-25 04:00:47 -0700

    question 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,5 @@ if number > 0:
     print("{:d} is postive".format(number))
 elif number ==0:
     print("{:d} is zero".format(number))
-elif number < 0:
+else:
     print("{:d} is negative".format(number))
