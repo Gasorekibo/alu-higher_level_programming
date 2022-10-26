a80eae2435cc56e86bf8eab2ba0735b504e4f375
Gasorekibo <m.gasore@alustudent.com>
2022-10-26 12:55:57 -0700

    question 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0,9):
-    for d in range(i+1,10):
+for i in range(0, 9):
+    for d in range(i+1, 10):
         if i == 8:
-            print("{}{}".format(i,d))
+            print("{}{}".format(i, d))
         else:
-            print("{}{}".format(i,d),end= ", ")
+            print("{}{}".format(i, d),end=", ")
