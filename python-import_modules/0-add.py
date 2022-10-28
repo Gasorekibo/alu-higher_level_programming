a6f259b13a201ba1bcc9967b2bfcbc32a13d0413
Gasorekibo <m.gasore@alustudent.com>
2022-10-28 13:27:23 -0700

    question 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-if __name__ == '__main__':
-    a = 1
-    b = 2
-    add(1,2)
-    print("{:d}+{:d}= {:d}".format(a,b,add), end="")
+a = 1   
+b= 2
+add(1,2)
+print("{:d}+{:d}= {:d}".format(a,b,add(1,2)), end="")
        
