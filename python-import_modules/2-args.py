ea049885edc1c4dbfcbe62727cf0b393693e3725
Gasorekibo <m.gasore@alustudent.com>
2022-10-28 15:03:16 -0700

    question 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    arguments = ["welcome", "to", "the", "Best", "School"]
-    print("{} aguments:".format(len(arguments)))
-    for i, m in enumerate(arguments):
-        print("{}: {}".format(i, m))
-
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{:d} arguments.".format(num - 1))
+    elif num == 2:
+        print("{:d} argument:".format(num - 1))
+    else:
+        print("{:d} arguments:".format(num - 1))
+    for i in range(1, num):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
