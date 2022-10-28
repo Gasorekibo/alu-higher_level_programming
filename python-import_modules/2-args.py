0920606fb0d9a1a2737c1ef67e7ab261de707332
Gasorekibo <m.gasore@alustudent.com>
2022-10-28 14:50:18 -0700

    question 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+arguments = ["welcome", "to", "the", "Best", "School"]
+print("{} aguments:".format(len(arguments)))
+for i, m in enumerate(arguments):
+    print("{}: {}".format(i, m))
+
