a0c736f8a6e01f74468aef7b6326553ac1b1035d
Gasorekibo <m.gasore@alustudent.com>
2022-10-31 04:03:39 -0700

    question 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ from sys import argv
 if __name__ == "__main__":
     length = len(argv)
     sum = 0
-    for i in argv(1,length):
-        sum = sum + int(argv[i])
+    for i in range(1, length):
+        sum += int(argv[i])
         print("{:d}".format(sum))
