5d2473e5a06fdd72ddd8a1a831e3e431f7c4b544
Gasorekibo <m.gasore@alustudent.com>
2022-10-31 03:59:31 -0700

    question 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    length = len(argv)
+    sum = 0
+    for i in argv(1,length):
+        sum = sum + int(argv[i])
+        print("{:d}".format(sum))
