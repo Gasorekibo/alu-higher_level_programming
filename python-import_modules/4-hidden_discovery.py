8859182b3460e677eb3d4653f5e8ea9989ac44e0
Gasorekibo <m.gasore@alustudent.com>
2022-11-01 03:09:29 -0700

    question 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 import hidden_4
 if __name__ == "__main__":
     for name in dir(hidden_4):
-        if name[:22]!= "__":
-                print("{}".format(name))
+        if name[:2] != "__":
+            print("{}".format(name))
