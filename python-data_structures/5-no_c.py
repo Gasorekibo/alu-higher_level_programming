733af3fb49757ee22c1da423fb31299d6109ca7d
Gasorekibo <m.gasore@alustudent.com>
2022-11-03 13:07:49 -0700

    question 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if ord('c'):
-        {ord('c') : None}
+        {ord('c'):None}
     elif ord('C'):
-        {ord('C') : None)
+        {ord('C'):None}
     else:
         return my_string
