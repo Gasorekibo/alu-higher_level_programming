8471afa8b42564a04c40b8dc26c8507f4be148fd
Gasorekibo <m.gasore@alustudent.com>
2022-11-03 14:36:50 -0700

    question 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,4 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        return del my_list[0]
+        del my_list[0]
+        return my_list
