4c1a4d05e03377afc26a2c43efbc04f7e34ff69e
Gasorekibo <m.gasore@alustudent.com>
2022-11-04 02:17:27 -0700

    question 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord('c'): None})
-    return my_string.translate({ord('C'): None})
+    return my_string.translate({ord('c'[0:]): None})
+return my_string.translate({ord('C'[0:]): None})
