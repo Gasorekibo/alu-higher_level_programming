ee1e93570ef9db8351215f85fe6875904206e0b2
Gasorekibo <m.gasore@alustudent.com>
2022-11-04 04:12:17 -0700

    question 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return None
-    else:
-        return sentence
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
