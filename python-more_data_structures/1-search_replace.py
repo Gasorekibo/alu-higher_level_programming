d2e795a1c754bf9a853c25a392ae3c26f8346434
Gasorekibo <m.gasore@alustudent.com>
2022-11-06 05:15:12 -0800

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
     for i in range(len(my_list)):
         new_list = my_list.copy()
         if new_list[i] == search:
