fc8a57ee818222d6175b57a0c56a602aa7c519cc
Gasorekibo <m.gasore@alustudent.com>
2022-11-06 05:36:29 -0800

    unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    new_list = set(my_list)
-    for i in range(len(new_list)):
-        sum = sum(new_list[i])
+    for i in set(my_list):
+        sum += i
     return sum
