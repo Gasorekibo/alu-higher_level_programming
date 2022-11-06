3292965ea2bc26e7419cf29c3d54b0dda9784bdc
Gasorekibo <m.gasore@alustudent.com>
2022-11-06 04:47:59 -0800

    square simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-new_matrix = [squares ** 2 for i in matrix for squares in i]
-print(new_matrix)
+    new_matrix = [squares ** 2 for i in matrix for squares in i]
+    print(new_matrix)
