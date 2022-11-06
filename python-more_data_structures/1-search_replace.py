6f004086c8feb8b913d7037ec315c0d7fbd90027
Gasorekibo <m.gasore@alustudent.com>
2022-11-06 05:11:27 -0800

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
-         new_list = my_list.copy()
-         if new_list == search:
-             new_list.append(replace)
+        new_list = my_list.copy()
+        if new_list[i] == search:
+            new_list.append(replace)
     return new_list
