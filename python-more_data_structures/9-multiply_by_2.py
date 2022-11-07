75875bb9d25ab58732ced3c92bdd767fa53ea8c9
Gasorekibo <m.gasore@alustudent.com>
2022-11-07 06:30:42 -0800

    multiply by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value*2
-    return a_dictionary
+    sorted_people = sorted(a_dictionary.items(), key=lambda item: item[1])
+    sorted_people_dict = {}
+    for key, value in sorted_people:
+        sorted_people_dict[key] = value*2
+    print(sorted_people_dict)
