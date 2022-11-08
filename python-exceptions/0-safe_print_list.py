e5dd4b7ccc5a75a58276b5478dd8e74a900990e8
Gasorekibo <m.gasore@alustudent.com>
2022-11-08 10:30:43 -0800

    question 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         length += 1
     for j in range(x):
         try:
-            print("{}".format(my_list(j)), end="")
+            print("{}".format(my_list[j]), end="")
             print("")
         except:
             return length
