176a7648930052729b5746ccf0fc31785935bd67
Gasorekibo <m.gasore@alustudent.com>
2022-11-08 13:33:38 -0800

    print and count intergres

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        if isinstance(x, int) and x <= range(my_list):
-            print("{:d}".format(x))
-        else:
-            pass
-    except Exception as ex:
-        print(ex)
+    length = 0
+    num = 0
+    for i in my_list:
+        length += 1
+    for j in range(x):
+        try:
+            if isinstance(j, int) and j <= range(x):
+                print("{:d}".format(m_list[j]), end="")
+            else:
+                pass
+        except:
+            print("")
+            return length
+        num += 1
+    print("")
+    return num
