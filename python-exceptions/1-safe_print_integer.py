72fda2f660f78835693d135ae528b1c6888110d0
Gasorekibo <m.gasore@alustudent.com>
2022-11-08 12:50:31 -0800

    safe print interger

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        for i in value:
-            if isinstance(i, int):
-                print("{:d}".format(i))
-                return True
-            else:
-                return False
+        if isinstance(value, int):
+            print("{:d}".format(i))
+            return True
+        else:
+            return False
     except Exception as ex:
         return(ex)
