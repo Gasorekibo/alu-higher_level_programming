a32d16b50ed4cdd826b12562eb1158dcd3dd6db3
Gasorekibo <m.gasore@alustudent.com>
2022-11-08 23:00:25 -0800

    safe print interger

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        def division(a,b):
+        def division(a, b):
             result = a/b
             return("Inside result: {}".format(result))
     except Exception as ex:
         pass
     finally:
         if b != 0:
-        division(a,b)
+            division(a, b)
         else:
             return None
