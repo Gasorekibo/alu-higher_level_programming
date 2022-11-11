954a3211b8f5db2974452d95f3f161bc87a300bb
Gasorekibo <m.gasore@alustudent.com>
2022-11-11 03:37:51 -0800

    question 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,9 +10,8 @@ class Square:
         '''checking the condition of attribute'''
         if size not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-    '''declaring a method that calculate the area of square'''
     def area(self):
-        Area = self.__size ** 2
-        return Area
+        '''return the area of square'''
+        return self.__size * self.__size
