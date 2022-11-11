106bf45e9bfb4981e49fd98604535a0804d790f1
Gasorekibo <m.gasore@alustudent.com>
2022-11-11 03:51:03 -0800

    question 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,10 @@ class Square:
     ''' instationing the private attribute'''
     
     def __init__(self, size=0):
-        '''checking the condition of attribute'''
+        ''' Args:
+
+                size (int): size of the new square
+        '''
         if size not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
