206ceb985a1bbcae4e81633781f3df93a3dcb741
Gasorekibo <m.gasore@alustudent.com>
2022-11-11 13:36:27 -0800

    question 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-# Printing a square
-""" defining square class """
+# a class Square that defines a square by: (based on 4-square.py)
+# but at this time we will add a private instance attribute
+'''
+   define a class 'Square'
+'''
 
 
 class Square:
-    """square class created"""
-    
+    '''
+       this class will contain __init__ module, and attributes
+    '''
     def __init__(self, size=0):
-        """instationing the attribute"""
+        ''' this is an __init__ method, it has characters of an object '''
         self.size = size
+
     @property
     def size(self):
-        """detter method to retrive the private attribute"""
-        return sef.__size
+        ''' this is a private instance attribute '''
+        return self.__size
+
     @size.setter
     def size(self, value):
-        """updating private attribute if it is interger and greater that zero"""
-        if not isinstance(size, int):
+        ''' stting the value of a size '''
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        elif value < 0:
+            raise ValueError("size must be >=0")
         self.__size = value
+
     def area(self):
-        """returning the are of square"""
-        return(self.__size * self.__size)
-    def def my_print(self):
-        """printing the square with the # if size != 0"""
+        ''' this function will return the area of the current square '''
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        ''' prints square with character # '''
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
