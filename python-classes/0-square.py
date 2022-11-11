41ef6e047c1b8997f33ac5e5cc4f4795c4864a68
Gasorekibo <m.gasore@alustudent.com>
2022-11-11 00:54:17 -0800

    My first square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-#Write an empty class Square that defines a square:
+# Write an empty class Square that defines a square:
 '''defining a simple class that does nothing
 '''
+
+
 class Square:
     '''writing a class
     '''
