393b63e0da6d838db0658ccdc479e41874181c70
Gasorekibo <m.gasore@alustudent.com>
2022-11-12 09:49:29 -0800

    question 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,13 +14,16 @@ class Rectangle:
         '''
         self.width = width
         self.height = height
+    
     @property
     def width(self):
         '''defining private attribute '''
         return self.__width
+    
     @width.setter
     def width(self, value):
-        ''' checking if the private attribute meet the condition to be retrieve
+        ''' checking if the private attribute meet
+        the condition to be retrieve
             args:
                 it has only one argument value'''
         if not isinstance(value, int):
@@ -28,14 +31,16 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+    
     @property
     def height(self):
         ''' retrieving the actual private atribute '''
         return self.__height
-
+    
     @height.setter
     def height(self, value):
-        ''' checking wether private attribute met the condition to be changed '''
+        ''' checking wether private attribute met
+        the condition to be changed '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
