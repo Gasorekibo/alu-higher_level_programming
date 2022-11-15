8aabbfd7e102101bec25242c693d888fa992d838
Gasorekibo <m.gasore@alustudent.com>
2022-11-15 13:23:26 -0800

    question 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-#a function that returns the list of available attributes and methods of an object
+# a function that returns the list of available 
+# attributes and methods of an object
 ''' question 0 '''
+
 
 def lookup(obj):
     ''' determine all atribute and method used '''
