868226bc337c83506c87e44319ff0653762c3ca7
Gasorekibo <m.gasore@alustudent.com>
2022-11-15 13:20:55 -0800

    question 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#a function that returns the list of available attributes and methods of an object
+''' question 0 '''
+
+def lookup(obj):
+    ''' determine all atribute and method used '''
+    return dir(obj)
