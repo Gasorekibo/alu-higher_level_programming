f326fcc2363fc49361f0fffc4b7d4d6c449a1f4e
Gasorekibo <m.gasore@alustudent.com>
2022-11-16 01:15:28 -0800

    question 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 # only subclass return True
 # and False if it is not a subclass
+''' we are not allowed to import module '''
 ''' we are going to us issubclass function '''
 
 
 
 def inherits_from(obj, a_class):
     ''' determine if obj is subclass of a_class '''
-    if issubclass(obj, a_class)):
+    if issubclass(type(obj), a_class)) and type(obj) != a_class:
         return True
     else:
         return False
