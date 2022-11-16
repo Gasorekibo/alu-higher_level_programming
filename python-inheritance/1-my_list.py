d7296ff94c8961b29bcab721e562516cde773197
Gasorekibo <m.gasore@alustudent.com>
2022-11-16 00:21:07 -0800

    question 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a class MyList that inherits from list
+''' list in sorted order '''
+
+
+def print_sorted(self):
+    ''' function that print sorted list '''
+    return(sorted(self))
