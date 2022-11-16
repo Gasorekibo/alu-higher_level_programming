4eaf456f2a902fba3165b7b9cc42d63222bde044
Gasorekibo <m.gasore@alustudent.com>
2022-11-16 00:59:45 -0800

    question 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Same class or inherit from
+
+
+def is_kind_of_class(obj, a_class):
+    ''' checking the condition '''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
