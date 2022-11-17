fcc0feb9faaebae9982b3ff0799bda8be3a13503
Gasorekibo <m.gasore@alustudent.com>
2022-11-17 01:37:34 -0800

    question 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-# Full rectangle
-# rectangle that inherit from BaseGeometry
-''' allowe to import module '''
-BaseGeometry = __import__(7-base_geometry.py).BaseGeometry
+# define Rectangle class
+# inherited from 8_BaseGeometry
+''' we allowed to import module '''
+Rectangle = __import__('7-base_geometry.py').Rectangle
+
 
 class Rectangle(BaseGeometry):
-    ''' instantioning Rectangle class '''
+    '''defining Rectaangle class '''
     def __init__(self, width, height):
-        ''' the method has two
-            args:
-              width and height
-        '''
+        """instationing class method"""
         self.integer_validator('width', width)
         self.__width = width
         self.integer_validator('height', height)
         self.__height = height
     def area(self):
-        ''' return the area of rectangle '''
-        return self.__width * self.__height
+        ''' return area of rectangle '''
+        return(self.__width * self__height)
     def __str__(self):
-        ''' printing is string format '''
-        return f'[Rectangle] {self.__width} / {self.__width}'
+        """ print string """
+        return f'[Rectangle] {self.__width}/{self.__height}'
