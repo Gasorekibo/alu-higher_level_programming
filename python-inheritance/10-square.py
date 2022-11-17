a4b9248ec97ab1e26a04bf12b149186bc9cb3053
Gasorekibo <m.gasore@alustudent.com>
2022-11-17 03:50:06 -0800

    question 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,18 +2,21 @@
 # class 'Square' that inherits from 'Rectangle'
 # (9-rectangle.py)
 """
-   define a class 'Square' inheriting from 'BaseGeometry'
+    define a class 'Square' inheriting from 'BaseGeometry'
 """
-Rectangle = __import__('9-rectangle.py').Rectangle
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ defining class 'Square'"""
+    """
+      representation of 'Square
+    """
+
     def __init__(self, size):
-        """ instantiation a new square
-           args:
-               size(int): the size of the class 'square'
         """
-        self.integer_validator("size", size)
+          instantiate private instance field size
+        """
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
