d7e58bb35f00fedea13309f442771c32487c3261
Gasorekibo <m.gasore@alustudent.com>
2022-11-17 01:47:14 -0800

    question 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # define Rectangle class
 # inherited from 8_BaseGeometry
-''' we allowed to import module '''
+
 BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 
