a3d18c1bbe9162aab7a462085df9a444bad9f9d4
Gasorekibo <m.gasore@alustudent.com>
2022-11-18 06:15:43 -0800

    question 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# append to a file
+''' a function that append to a file and return
+   number of characters added '''
+
+
+   def append_write(filename="", text=""):
+       ''' a function that append to a file '''
+       with open(filename, 'a', encoding='utf-8') as f:
+           return f.write(text)
