a12a16ae9c1031d512b6c05b537f1f94c8df6a6f
Gasorekibo <m.gasore@alustudent.com>
2022-11-18 11:12:46 -0800

    question 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Student to JSON
+#  a class Student that defines a student
+''' defining a class with puplic instance '''
+import json
+
+
+class Student:
+    ''' defining a class with public instance '''
+    def __init__(self, first_name, last_name, age):
+        ''' instation of the class method '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        ''' convert to json '''
+        return json.load(__dict__)
