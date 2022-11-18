0a48066f10df959743362a438d7fa062604a0cc1
Gasorekibo <m.gasore@alustudent.com>
2022-11-18 06:05:05 -0800

    question 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# write to a file
+# writing string to a file
+''' starting by writing a fucntion '''
+
+
+def write_file(filename="", text=""):
+    ''' a function that write a text to a fie '''
+    with open(filename, 'w', encoding='utf-8')as f:
+        return f.write('text')
