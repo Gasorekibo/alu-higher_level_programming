58c13fe0c7f8d43188331cf7e6b3b03532a2c60c
Gasorekibo <m.gasore@alustudent.com>
2022-11-18 06:25:18 -0800

    question 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# to json object
+# we are going to return json string
+''' defining function that do it '''
+
+
+def to_json_string(my_obj):
+    ''' a function that convert dict to json object '''
+    return json.dumps(obj)
