10a7a22e236632b688d33b206b060b1f7cb859f7
Gasorekibo <m.gasore@alustudent.com>
2022-11-25 12:29:56 -0800

    question 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 -- including some foreign keys
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-id INT NOT NULL AUTO-INCREMENT,
+id INT NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
-FOREIGN KEY(state_id) REFERENCE hbtn_0d_usa.states (id)
-);
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states (id)
