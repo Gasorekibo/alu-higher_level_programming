85301f147e77cc83fcd13754b11cf408b7798b90
Gasorekibo <m.gasore@alustudent.com>
2022-12-01 07:42:52 -0800

    question 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value*2
+def multiply_by_2(a_dictionary:
+    for keys, values in a_dictionary.items():
+        a_dictionary[keys] = value*2
     return a_dictionary
+
+
+
+
