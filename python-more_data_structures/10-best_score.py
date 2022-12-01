f5862f77f3a4784a6e078d155594b000c558e366
Gasorekibo <m.gasore@alustudent.com>
2022-12-01 08:22:00 -0800

    question 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for value, key in a_dictionary.items():
-        if value in a_dictionary:
-        a_dictionary[key] = max(value)
-    return a_dictionary
-    else:
-        return None
-
+    return max(a_dictionary)
