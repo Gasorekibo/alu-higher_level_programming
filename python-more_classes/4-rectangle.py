0809a8b8a1608b8a6e8f63eaad786462e48019d6
Gasorekibo <m.gasore@alustudent.com>
2022-12-02 02:33:45 -0800

    question 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
                 width(int): it is a width of rectangle
                 rectangle(int): it is a height of rectangle
         '''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
