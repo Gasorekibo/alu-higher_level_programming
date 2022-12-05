169310e39739692c010708ead341538bb35ee39e
Gasorekibo <m.gasore@alustudent.com>
2022-12-05 13:43:18 -0800

    question 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# python network
+''' fetching a given url '''
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alu-intranet.hbtn.io/status")
+    data = with urllib.request.urlopen(req)as f:
+        body = f.read()
+        print("Body response:")
+        print("\t -type : {}".format(type(body)))
+        print("\t -content: {}".format(body))
+        print("\t -utf8 content: {}".format(body.decode("utf-8")))
+
+
