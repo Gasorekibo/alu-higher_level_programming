abc3824a10f7baef0a80fd797ee7b2f7b8eaad21
Gasorekibo <m.gasore@alustudent.com>
2022-12-06 05:21:47 -0800

    question 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,11 +3,11 @@
 """
     the script fetches https://alu-intranet.hbtn.io/status
 """
-import request
+import requests
 
 
 if __name__ == "__main__":
-    result = request.get("https://alu-intranet.hbtn.io/status")
+    result = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(result)))
     print("\t- content: {}".format(result))
