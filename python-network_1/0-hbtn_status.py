8f5906ce21fa34c80eedcb65efabd7fd4c8bda72
Gasorekibo <m.gasore@alustudent.com>
2022-12-06 02:49:45 -0800

    question 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# fetching a specific url given
+""" we are going to fetch https://alu-intranet.hbtn.io/status as url given """
+from urllib.request import urllopen
+
+
+if __name__ == "__main__":
+    with urlopen("https://alu-intranet.hbtn.io/status") as f:
+        value = f.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(value)))
+        print("\t - content: {}".format(value))
+        print("\t - utf8 content: {}".format(value.decode("utf-8")))
