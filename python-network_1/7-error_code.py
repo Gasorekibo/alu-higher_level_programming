cf78afe3ada73c0e8e6d54369ef10b022c0b0f07
Gasorekibo <m.gasore@alustudent.com>
2022-12-06 06:09:05 -0800

    question 7

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#  a Python script that takes in a URL, sends a request to the URL
+"""
+    and displays the body of the response using requests package
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[2]
+    result = requests.get(url)
+    if result.status-code >= 400:
+        print("Error code: {}".format(result.status_code))
+    else:
+        print(result.tex)
