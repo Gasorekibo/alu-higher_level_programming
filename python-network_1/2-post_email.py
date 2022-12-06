c4f70ea7727ade3181fe8da4b2a47654cfc9b6c5
Gasorekibo <m.gasore@alustudent.com>
2022-12-06 04:52:10 -0800

    question 2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/in/python3
+# a script that takes in URL and email and post request to URL,
+"""
+    and displays content of the head decoded in utf-8
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    info = urllib.parse.urlencode(email)
+    info = data.encode("ascii")
+    req = urllib.request.Request(url, info)
+    with urllib.request.urlopen(req) as f:
+        result = read()
+        print(result.decode("utf-8")
+
