f9ec6d62c822b2887078f57c5401a594e66c09d7
Gasorekibo <m.gasore@alustudent.com>
2022-12-06 03:39:50 -0800

    question 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# dispaying the head of the response
+""" importing two module sys and urllib.reques """
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as f:
+        result = f.read()
+        print(result.headers["X-Request-Id")
