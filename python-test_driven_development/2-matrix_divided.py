953d17e83e7fbe23d54dfd7ecbfd6acc389d27c3
Gasorekibo <m.gasore@alustudent.com>
2022-12-08 06:09:07 -0800

    question 1 matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,4 +11,21 @@ def matrix_divided(matrix, div):
             matrix: a list of integer and div which is the element to be 
             divided to the matrix
     """
-    
+    new_matrix = []
+    if (not isinstance(matrix, list) or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all((isinstance(value, int) or isinstance(value, float))
+            for value in [num for row in matrix for num in row])):
+                raise TypeError("matrix must be a matrix (list of lists) of "
+                    "integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    else:
+        for i in matrix:
+            result = list(map(lambda x : round(x/div, 2), i))
+            new_matrix.append(result)
+        return(new_matrix)
