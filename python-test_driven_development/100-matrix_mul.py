fbd42d6eeee27aed2ba8e0df453c84618628fb8d
Gasorekibo <m.gasore@alustudent.com>
2022-12-11 10:11:19 -0800

    Advaned task

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -37,7 +37,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    new_row = []
+        
+    product = []
+    for r in range(len(m_b[0])):
+        new_row = []
         for c in range(len(m_b)):
             new_row.append(m_b[c][r])
         product.append(new_row)
