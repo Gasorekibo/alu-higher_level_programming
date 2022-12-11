12e5cc6aa89eeff83b741501498a8291469daa04
Gasorekibo <m.gasore@alustudent.com>
2022-12-11 04:43:45 -0800

    adva

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -8,7 +8,7 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """
         argument: m_a which is matrix
-                  m_b which is also a matrix 
-                  this return m_a * m_b
+        m_b which is also a matrix
+        this return m_a * m_b
     """
     return (np.matmul(m_a, m_b))
