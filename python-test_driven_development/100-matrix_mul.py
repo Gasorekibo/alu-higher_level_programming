bc22b810b06e0a2ce7cf22b49098f5bd4b5d203f
Gasorekibo <m.gasore@alustudent.com>
2022-12-11 10:20:48 -0800

    Advanced task

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -26,10 +26,10 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
     if not all((isinstance(value, int) or isinstance(value, float))
-            for value in [num for row in m_a for num in row]):
+              for value in [num for row in m_a for num in row]):
         raise TypeError("m_a should contain only integers or floats")
     if not all(isinstance(value, int) or isinstance(value, float)
-            for value in [num for row in m_b for num in row]):
+              for value in [num for row in m_b for num in row]):
         raise TypeError("m_b should contain only integers or floats")
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
