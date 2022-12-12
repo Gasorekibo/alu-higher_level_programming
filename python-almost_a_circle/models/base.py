6242910c9101caddd5952695163ae70045332f5f
Gasorekibo <m.gasore@alustudent.com>
2022-12-12 12:09:43 -0800

    question 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    a base class that inherit from object
+"""
+
+
+class Base:
+    """
+        a class with private class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            a instance method with one instace attribute
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects+=1
+            self.id = Base.__nb_objects
