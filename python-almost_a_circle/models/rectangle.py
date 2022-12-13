8ad982bb63408b7b7113cdfeddbb845902db37f3
Gasorekibo <m.gasore@alustudent.com>
2022-12-13 14:12:43 -0800

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,8 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ dictionary representation of rectangle class """
+        return {"width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
