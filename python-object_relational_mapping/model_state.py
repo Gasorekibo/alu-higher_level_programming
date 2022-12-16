254fc58d3a71f8d6e490041184094e2aed457144
Gasorekibo <m.gasore@alustudent.com>
2022-12-16 02:10:22 -0800

    creating class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    python file that contains the class definition of a State
+    and an instance
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ class city inherit from Base """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
