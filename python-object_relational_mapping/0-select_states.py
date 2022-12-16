823cd0e7575394d60244126ed0e04ddd923156ec
Gasorekibo <m.gasore@alustudent.com>
2022-12-16 01:10:14 -0800

    list all states from database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,8 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    database = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                               db=sys.argv[3])
     c = database.cursor()
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]
