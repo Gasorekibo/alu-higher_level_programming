eeb7c715d05e0fd5908b4803429059f5a88e4340
Gasorekibo <m.gasore@alustudent.com>
2023-02-06 11:28:29 -0300

    Javascript warm up

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const arguments = process.argv.slice(2);
-const value = parseInt(arguments[0]);
+const args = process.argv.slice(2);
+const value = parseInt(args[0]);
 if (value === undefined) {
   console.log('Not a number');
 } else if (isNaN(value)) {
