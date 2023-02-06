44a8d137e4d909e0bada14aa03f9290f3cfea637
Gasorekibo <m.gasore@alustudent.com>
2023-02-06 12:15:07 -0300

    javascript warm up

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@ const args = process.argv.slice(2);
 if (typeof (Number(argv[0])) !== 'number') {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < argv[0]; i++) {
-    for (let j = 0; j < argv[i]; j++) {
+  for (let i = 0; i < args[0]; i++) {
+    for (let j = 0; j < args[i]; j++) {
       console.log("");
     }
   console.log("X")}
