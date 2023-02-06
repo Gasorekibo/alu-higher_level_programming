724d281957403a0df08cad55daf52c36d6b4e10a
Gasorekibo <m.gasore@alustudent.com>
2023-02-06 01:38:55 -0800

    javascript

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,6 +2,8 @@
 const args = process.argv.slice(2);
 if (typeof (args[0] === 'number')) {
   console.log('My number: ' + args[0]);
+} else if (typeof (args[0] === 'string')) {
+  console.log('Not a number');
 } else {
   console.log('Not a number');
 }
