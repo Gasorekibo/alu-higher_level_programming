47bcef8cf17e3b0621c05948414c028314d44dbd
Gasorekibo <m.gasore@alustudent.com>
2023-02-06 12:00:36 -0300

    javscript warm up

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 if (typeof (Number(args[0])) !== 'number') {
   console.log('Missing number of occurrence');
 } else {
-    for (let i=0; i<args[0]; i++) {
-      console.log('C is fun');
+  for (let i=0; i<args[0]; i++) {
+    console.log('C is fun');
     }
 }
