564cc37951521a51f36adf46e4ad78af414bf60e
Gasorekibo <m.gasore@alustudent.com>
2023-02-06 06:01:18 -0300

    javascript warm up

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const argument = process.argv.slice(2);
 if (argument.length === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argument.length === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
 
