f0807cbc65953394783319b484f586b93f07af5b
Gasorekibo <m.gasore@alustudent.com>
2023-02-07 04:03:43 -0300

    javascript warm up

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function factorial(args) {
+  result = 1;
+  if (typeof (args) === 'number') {
+    for (let i = 1; i<= args; i++) {
+        result = 1 * i;
+    }
+    } else if (Number(args) === NaN) {
+        result = 1;
+    } else {
+        console.log('No answer');
+    }
+console.log(result);
+}
+
+factorial();
