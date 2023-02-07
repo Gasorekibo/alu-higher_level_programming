a7c8ed878947f77c5a4c76c3294a0decc47e239c
Gasorekibo <m.gasore@alustudent.com>
2023-02-07 17:40:44 -0300

    Advanced javascript warm up

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,13 +2,11 @@
 const myObject = {
   type: 'object',
   value: 12
-  };
+};
 console.log(myObject);
-  
 myObject.incr = function () {
   this.value++;
   };
-  
 myObject.incr();
 console.log(myObject);
 myObject.incr();
