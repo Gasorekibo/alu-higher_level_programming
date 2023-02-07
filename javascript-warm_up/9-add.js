0f44ca929f60f4fb12a21b4667cacaf4b43d9999
Gasorekibo <m.gasore@alustudent.com>
2023-02-07 03:46:29 -0300

    Javascript warm up

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 function add(a, b) {
   if (typeof (a) === undefined || typeof (b) === undefined) {
     console.log('NaN');
-  } else if (typeof (a) === 'number' && typeof (b) === 'number') {
+} else if (typeof (a) === 'number' && typeof (b) === 'number') {
     console.log(a + b);
   } else {
     console.log('NaN');
