d828dc32b12b2da01b4a8ba08513226365ce1711
Gasorekibo <m.gasore@alustudent.com>
2023-02-08 14:15:06 -0300

    js object scope and closure

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+}
     
-    print () {
-      for (let value = 1; value <= this.height; value++) {
-        console.log('X'.repeat(this.width));
-      }
+  print () {
+    for (let value = 1; value <= this.height; value++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 
-    rotate () {
-        [this.height, this.width] = [this.width, this.height];
-    }
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
 
-    double () {
-        2[this.height, this.width];
-    }
-  };
+  double () {
+    [this.height, this.width] = [this.height * 2, this.width* 2];
+  }
+};
