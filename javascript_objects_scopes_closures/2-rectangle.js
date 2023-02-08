5773ea864605f41b1282041de55fd04f3b742cee
Gasorekibo <m.gasore@alustudent.com>
2023-02-08 13:28:19 -0300

    Js object class and closure

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+
+};
+if (this.height <= 0 || this.width <= 0) {
+    const newObject = new Rectangle();
+}
