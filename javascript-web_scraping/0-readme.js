1e20a812f4faa2f8dc55bcb669507f13c3960bde
Gasorekibo <m.gasore@alustudent.com>
2023-02-09 18:38:56 -0300

    reading file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile('Readme', 'utf-8', function (err, data) {
+const filePath = process.argv[2];
+fs.readFile('filepath', 'utf-8', function (err, data) {
   if (err) {console.log(err)}
   console.log(data);
   }
