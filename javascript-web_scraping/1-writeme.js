ccbce17156277dea22708113fe6ef1716ad37296
Gasorekibo <m.gasore@alustudent.com>
2023-02-10 07:31:01 -0300

    js web scrapting

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const Path = process.argv[3];
-fs.writeFile(Path, 'hello', (error, content) => {
+const Path = process.argv[2];
+const content = process.argv[3]
+fs.writeFile(Path, content, 'utf-8', (error, content) => {
   if (error) {
     console.log(error); }
-  console.log(content.toString());
+  console.log(content);
 })
