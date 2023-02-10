e312d57f09292c12b4dc06ecf9baf1dd447b4dab
Gasorekibo <m.gasore@alustudent.com>
2023-02-10 10:29:40 -0300

    js web scraping

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,6 +2,6 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, (error, data) => {
-  if (error) { console.log(error) }
+  if (error) { console.log(error); }
   console.log(JSON.parse(data.body).title);
 });
