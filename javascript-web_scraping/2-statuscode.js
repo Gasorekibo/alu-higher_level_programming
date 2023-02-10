4658b3bbdc2750d3af62180c10d8d7662f6785ec
Gasorekibo <m.gasore@alustudent.com>
2023-02-10 07:51:06 -0300

    js finding the status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response) => {
+request.get(url, (error, response) => {
   if (error) { console.log(error); }
-  console.log('code: ' + response.statuscode);
+  else {
+    console.log('code: ' + response.statuscode);
+  }
 });
