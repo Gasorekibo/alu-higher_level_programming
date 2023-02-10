8a58a39c96ad7957ba84df626504470be8b246e8
Gasorekibo <m.gasore@alustudent.com>
2023-02-10 07:33:23 -0300

    js writting to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const Path = process.argv[2];
-const content = process.argv[3]
+const content = process.argv[3];
 fs.writeFile(Path, content, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error); }
-  console.log(content);
-})
+  if (error) {console.log(error); }
+});
